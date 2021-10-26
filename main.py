825720a1d685a28d9eff6daabf6b713759f4c774
Abiral Shakya <>
2021-10-26 22:16:25 +0000

    adding api call to dog.ceo

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+import requests as re 
+
+url_host = "https://dog.ceo"
+endpoint = "/api/breeds/list/all"
+query = ""
+
+url = url_host + endpoint + query 
+
+response = re.get(url)
+
+print(response.json())
