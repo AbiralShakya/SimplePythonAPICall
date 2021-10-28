a9fd580abb63e7d9d9fb538649cbd6c11abe411f
Abiral Shakya <91761753+AbiralShakya@users.noreply.github.com>
2021-10-28 21:55:54 +0000

    prettifying the display of dog breeds

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ url = url_host + endpoint + query
 
 response = re.get(url)
 
-print(response.json())
+breeds = response.json()["message"]
+#bulldogs = breeds.get("bulldog")
+#print(bulldogs)
+
+all_breeds = breeds.keys()
+for breed in all_breeds:
+  print(breed)
+
+#get list of all subbreeds of breed with at least 3 sub-breeds 
+
+hounds = breeds["hound"]
+input("\nPress enter to get all hound subbreeds")
+for hound in hounds:
+  print(hound)
+
